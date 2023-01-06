576a9c473bdc0e4a741ff932e0d77dd34ebc5602
iuliana <iuliana-stefania.cuptor@student.tuiasi.ro>
2023-01-06 14:59:32 +0200

    spring project

---
+++ Spring/proiect/src/test/java/com/licenta/proiect/ProiectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.proiect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
