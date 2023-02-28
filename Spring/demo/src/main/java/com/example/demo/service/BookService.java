6a1669102d043d9664b8ecfed7e12b55d8b78e4d
iuliana <iuliana-stefania.cuptor@student.tuiasi.ro>
2023-02-28 19:45:14 +0200

    test

--- Spring/demo/src/main/java/com/example/demo/service/BookService.java
+++ Spring/demo/src/main/java/com/example/demo/service/BookService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.BooksRepository;
 import com.example.demo.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+    @Autowired
+    private BooksRepository booksRepository;
+
     @Autowired
     private UsersRepository usersRepository;
 }
